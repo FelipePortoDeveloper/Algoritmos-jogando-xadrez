1635cdbed909aa9cfe78bc36b3b05f0d93f1df03
FelipePortoDeveloper <felipeprates49@gmail.com>
2025-03-14 21:15:43 -0300

    Add files via upload

--- xadrez.py
+++ xadrez.py
@@ -8,7 +8,8 @@ from ias.randomIA import random_move
 
 ALTURA, LARGURA = 720, 720
 TAM_QUADRADOS = ALTURA / 8
-IASELECIONADA = "Random"
+
+ia_vs_ia = True
 
 BRANCO, VERDE = (238, 238, 210), (118, 150, 86)
 
@@ -41,6 +42,58 @@ def draw_board():
 
 
 def evaluate_board(board):
+    valores_pos = {"P": [[0, 0, 0, 0, 0, 0, 0, 0], 
+                         [50, 50, 50, 50, 50, 50, 50, 50], 
+                         [10, 10, 20, 30, 30, 20, 10, 10], 
+                         [5, 5, 10, 25, 25, 10, 5, 5], 
+                         [0, 0, 0, 20, 20, 0, 0, 0], 
+                         [5, -5, -10, 0, 0, -10, -5, 5], 
+                         [5, 10, 10, -20, -20, 10, 10, 5], 
+                         [0, 0, 0, 0, 0, 0, 0, 0]], 
+                    "N": [[-50, -40, -30, -30, -30, -30, -40, -50],
+                        [-40, -20, 0, 0, 0, 0, -20, -40], 
+                        [-30, 0, 10, 15, 15, 10, 0, -30],
+                        [-30, 5, 15, 20, 20, 15, 5, -30],
+                        [-30, 0, 15, 20, 20, 15, 0, -30],
+                        [-30, 5, 10, 15, 15, 10, 5, -30],
+                        [-40, -20, 0, 5, 5, 0, -20, -40],
+                        [-50, -40, -30, -30, -30, -30, -40, -50]], 
+                    "B": [[-20, -10, -10, -10, -10, -10, -10, -20],
+                        [-10, 0, 0, 0, 0, 0, 0, -10],
+                        [-10, 0, 5, 10, 10, 5, 0, -10],
+                        [-10, 5, 5, 10, 10, 5, 5, -10],
+                        [-10, 0, 10, 10, 10, 10, 0, -10],
+                        [-10, 10, 10, 10, 10, 10, 10, -10],
+                        [-10, 5, 0, 0, 0, 0, 5, -10],
+                        [-20, -10, -10, -10, -10, -10, -10, -20]],
+                    "R": [[0, 0, 0, 0, 0, 0, 0, 0],
+                        [5, 10, 10, 10, 10, 10, 10, 5],
+                        [-5, 0, 0, 0, 0, 0, 0, -5],
+                        [-5, 0, 0, 0, 0, 0, 0, -5],
+                        [-5, 0, 0, 0, 0, 0, 0, -5],
+                        [-5, 0, 0, 0, 0, 0, 0, -5],
+                        [-5, 0, 0, 0, 0, 0, 0, -5],
+                        [0, 0, 0, 5, 5, 0, 0, 0]],
+                    "Q": [[-20, -10, -10, -5, -5, -10, -10, -20],
+                        [-10, 0, 0, 0, 0, 0, 0, -10],
+                        [-10, 0, 5, 5, 5, 5, 0, -10],
+                        [-5, 0, 5, 5, 5, 5, 0, -5],
+                        [0, 0, 5, 5, 5, 5, 0, -5],
+                        [-10, 5, 5, 5, 5, 5, 0, -10],
+                        [-10, 0, 5, 0, 0, 0, 0, -10],
+                        [-20, -10, -10, -5, -5, -10, -10, -20]],
+                    "K": [[0, 0, 0, 0, 0, 0, 0, 0],
+                        [0, 0, 0, 0, 0, 0, 0, 0],
+                        [0, 0, 0, 0, 0, 0, 0, 0],
+                        [0, 0, 0, 0, 0, 0, 0, 0],
+                        [0, 0, 0, 0, 0, 0, 0, 0],
+                        [0, 0, 0, 0, 0, 0, 0, 0],
+                        [0, 0, 0, 0, 0, 0, 0, 0],
+                        [0, 0, 0, 0, 0, 0, 0, 0]]
+                    }
+    
+    valores_pos_pretas = {piece: values[::-1] for piece, values in valores_pos.items()}
+
     valores = {"P": 10, "N": 30, "B": 30, "R": 50, "Q": 90, "K": 900}
     score = 0
 
@@ -53,19 +106,21 @@ def evaluate_board(board):
     if board.is_stalemate() or board.is_insufficient_material():
         return -100
 
-    central_squares = [chess.D4, chess.E4, chess.D5, chess.E5]
-
     for square in chess.SQUARES:
         peca = board.piece_at(square)
+
         if peca:
             valor_peca = valores.get(peca.symbol().upper(), 0)
+            linha, coluna = divmod(square, 8)
+
             if peca.color == chess.WHITE:
                 score -= valor_peca
+
             else:
                 score += valor_peca
 
-            if square in central_squares:
-                score -= 5 if peca.color == chess.WHITE else +5
+                if peca.symbol().upper() in valores_pos_pretas:
+                    score += valores_pos_pretas[peca.symbol().upper()][linha][coluna]
 
     score += len(list(board.legal_moves)) * 0.1
 
@@ -130,7 +185,7 @@ def move_ai(selecionada: str):
     elif selecionada == "Min":
         for move in board.legal_moves:
             board.push(move)
-            valor = minimax(board, 2, float('-inf'), float('inf'), False)
+            valor = minimax(board, 1, float('-inf'), float('inf'), False)
             board.pop()
 
             if valor > melhor_valor:
@@ -143,7 +198,7 @@ def move_ai(selecionada: str):
 rodando = True
 board = chess.Board()
 relogio = pygame.time.Clock()
-pecaSelecionada = None
+quadradoSelecionado = None
 pausa = False
 
 
@@ -173,18 +228,45 @@ while rodando:
                 if pygame.key.get_pressed()[pygame.K_DOWN]:
                     print(board)
 
+                if event.type == pygame.MOUSEBUTTONDOWN and board.turn == chess.WHITE and ia_vs_ia == False:
+                    x, y = event.pos
+
+                    coluna = int(x // TAM_QUADRADOS)
+                    linha = 7 - int(y // TAM_QUADRADOS)
+
+                    quadrado = chess.square(coluna, linha)
+                    peca = board.piece_at(quadrado)
+
+                    if quadradoSelecionado == None:
+                        quadradoSelecionado = quadrado
+
+                    else:
+
+                        move = chess.Move(quadradoSelecionado, quadrado)
+
+                        if move in board.legal_moves:
+                            board.push(move)
+                        else:
+                            quadradoSelecionado = None
+
+                        quadradoSelecionado = None
+                            
+                                                         
+
+                    
+
         
-        if not pausa:
-
-            if board.turn == chess.WHITE:
-                move = move_ai("Random")
-            else:
+        if not pausa:       
+            if board.turn == chess.BLACK:
                 move = move_ai("Min")
-               
-                
             
+                if move:
+                    board.push(move)
+            
+            else:
+                move = move_ai("Random")
 
-            if move:
-                board.push(move)
+                if move:
+                    board.push(move)
 
 
